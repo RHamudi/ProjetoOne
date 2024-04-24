6484cbb7473bf524dcf3a5439d26a17be12a6c1d
Hamudi <ramonramos.silva16@gmail.com>
2024-04-24 17:44:30 -0300

    Removendo import desnecessario

--- src/com/alura/conversor/front/Main.java
+++ src/com/alura/conversor/front/Main.java
@@ -1,8 +1,5 @@
 package com.alura.conversor.front;
 
-import com.alura.conversor.calculator.Converter;
-import com.alura.conversor.request.RequestAPI;
-
 public class Main {
     public static void main(String[] args) {
         UserInterface.SelectCoin();
