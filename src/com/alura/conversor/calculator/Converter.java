e515d468223e144baaa59f7aa0ad305b876ff76f
Hamudi <ramonramos.silva16@gmail.com>
2024-04-24 17:44:02 -0300

    Adicionando historico de conversões

--- src/com/alura/conversor/calculator/Converter.java
+++ src/com/alura/conversor/calculator/Converter.java
@@ -2,34 +2,64 @@ package com.alura.conversor.calculator;
 
 import com.alura.conversor.request.RequestAPI;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Converter {
+    private static Map<String, Double> convertedValues = new HashMap<>();
+
+    public static void imprimirValoresConvertidos() {
+        System.out.println("*****************************************************");
+        System.out.println("Exibindo seu historico de conversões");
+        if(convertedValues.isEmpty()){
+            System.out.println("Você precisa realizar alguma conversão antes de escolher essa opção!");
+        } else {
+            for (Map.Entry<String, Double> entry : convertedValues.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        }
+        System.out.println("*****************************************************");
+    }
+
     public static double ConvertUSDtoBRL(double inputValue){
         double valueCambio = RequestAPI.RequestExchange("USD", "BRL");
-        return inputValue * valueCambio;
+        double valueConverted = inputValue * valueCambio;
+        convertedValues.put("USD para BRL", valueConverted);
+        return valueConverted;
     }
 
     public static double ConvertBOBtoUSD(double inputValue){
         double valueCambio = RequestAPI.RequestExchange("BOB", "USD");
-        return inputValue * valueCambio;
+        double valueConverted = inputValue * valueCambio;
+        convertedValues.put("BOB para USD", valueConverted);
+        return valueConverted;
     }
 
     public static double ConvertARStoCLP(double inputValue){
         double valueCambio = RequestAPI.RequestExchange("ARS", "CLP");
-        return inputValue * valueCambio;
+        double valueConverted = inputValue * valueCambio;
+        convertedValues.put("ARS para CLP", valueConverted);
+        return valueConverted;
     }
 
     public static double ConvertBRLtoCOP(double inputValue){
         double valueCambio = RequestAPI.RequestExchange("BRL", "COP");
-        return inputValue * valueCambio;
+        double valueConverted = inputValue * valueCambio;
+        convertedValues.put("BRL para COP", valueConverted);
+        return valueConverted;
     }
 
     public static double ConvertCOPtoARS(double inputValue){
         double valueCambio = RequestAPI.RequestExchange("COP", "ARS");
-        return inputValue * valueCambio;
+        double valueConverted = inputValue * valueCambio;
+        convertedValues.put("COP para ARS", valueConverted);
+        return valueConverted;
     }
 
     public static double ConvertCLPtoBOB(double inputValue){
         double valueCambio = RequestAPI.RequestExchange("CLP", "BOB");
-        return inputValue * valueCambio;
+        double valueConverted = inputValue * valueCambio;
+        convertedValues.put("CLP para BOB", valueConverted);
+        return valueConverted;
     }
 }
